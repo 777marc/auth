4a3ccf212c62d9082eb7d51e9992bc01696dfa5a
777marc@gmail.com <777marc@gmail.com>
2022-01-25 19:19:24 -0500

    adding get current user

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,21 @@
 	import { onMount } from 'svelte';
 
 	let data = [];
+	let name = '';
 
 	onMount(async () => {
-		const res = await fetch(`http://localhost:3001/api/users/currentuser`);
-		data = await res.json();
+		const token = localStorage.getItem('jwt');
+		let headers = {};
+		if (token) {
+			headers['Authorization'] = `Bearer ${token}`;
+		}
+		const res = await fetch(`http://localhost:3001/api/users/currentuser`, { headers });
+		if (res.ok) {
+			data = await res.json();
+			name = data.currentUser.name;
+		}
 	});
 </script>
 
 <h1>profile page</h1>
-{JSON.stringify(data)}
+{name}
