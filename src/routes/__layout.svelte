2b130192333971ff409de3f72360f393bad689d1
Marc Mendoza <777marc@gmail.com>
2022-01-28 15:25:44 -0500

    nav work

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,23 +1,33 @@
 <script>
 	import '../tailwind.css';
+	let current = 'dashboard';
+	let active =
+		'inline-block border border-white rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white';
+	let inactive =
+		'inline-block border border-white bg-gray-400 text-white rounded hover:border-gray-200 hover:bg-gray-200 py-2 px-4';
 </script>
 
 <div class="w-full min-h-screen bg-gray-200 px-10 md:px-20 lg:px-52 py-20">
 	<ul class="flex justify-between pb-8">
 		<li class="mr-3">
 			<a
-				class="inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white"
+				class={current === 'dashboard' ? active : inactive}
+				on:click={() => (current = 'dashboard')}
 				href="/">home</a
 			>
 		</li>
 		<li class="mr-3">
 			<a
-				class="inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4"
+				class={current === 'profile' ? active : inactive}
+				on:click={() => (current = 'profile')}
 				href="/user">profile</a
 			>
 		</li>
 		<li class="mr-3">
-			<a class="inline-block py-2 px-4 text-gray-400 cursor-not-allowed" href="/auth/login">login</a
+			<a
+				class={current === 'login' ? active : inactive}
+				on:click={() => (current = 'login')}
+				href="/auth/login">login</a
 			>
 		</li>
 	</ul>
