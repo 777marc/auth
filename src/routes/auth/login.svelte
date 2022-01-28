ddd1541b0aeeb2cb17038b0f4c336bd633134df3
Marc Mendoza <777marc@gmail.com>
2022-01-28 15:58:56 -0500

    misc

--- src/routes/auth/login.svelte
+++ src/routes/auth/login.svelte
@@ -5,17 +5,32 @@
 
 	let email = '';
 	let password = '';
+	let showErrors = false;
+	let error = {
+		id: '',
+		message: ''
+	};
 
 	async function login() {
-		const res = await post(fetch, 'http://localhost:3001/api/users/signin', {
-			email,
-			password
-		});
+		showErrors = false;
 
-		if (res.ok) {
-			let data = await res.json();
-			browserSet('jwt', data.token);
-			goto('/dashboard');
+		try {
+			const res = await post(fetch, 'http://localhost:3001/api/users/signin', {
+				email,
+				password
+			});
+			if (res.ok) {
+				let data = await res.json();
+				browserSet('jwt', data.token);
+				goto('/');
+			} else {
+				showErrors = true;
+			}
+		} catch (err) {
+			error.id = err.id;
+			error.message = err.message;
+			showErrors = true;
+			console.log(error);
 		}
 	}
 </script>
@@ -57,4 +72,22 @@
 			Forgot Password?
 		</a>
 	</div>
+
+	{#if showErrors}
+		<div
+			class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mt-4"
+			role="alert"
+		>
+			<strong class="font-bold">Error - {error.id}</strong>
+			<span class="block sm:inline">: {error.message}</span>
+			<span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+				<svg
+					class="fill-current h-6 w-6 text-red-500"
+					role="button"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 20 20"
+				/></span
+			>
+		</div>
+	{/if}
 </div>
